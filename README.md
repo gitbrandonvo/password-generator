c6deaf08766910d9dd707dbd4ee2e04df3130677
Brandon Vo <69447326+gitbrandonvo@users.noreply.github.com>
2022-08-21 01:03:38 -0600

    Stretch goals implemented + Updated README.md
    Completed stretch goals (challenges):
    - Ability to set password length
    - Add "copy-on-click"
    - Toggle "symbols" and "numbers" on/off + letters

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# password-generator
- A simple password generator tasked by Scrimba
+# Password Generator
+Project tasked by Scrimba.com
+
+A simple password generator
+
+Online deployment (via Netlify): https://brandonvo-pass-generator.netlify.app/
+
+Requirements ("Completed requirements" commit):
+
+- Built from "scratch" (initial commit) ✔
+- Follows design ✔ 
+- Generate two random passwords when user clicks the button ✔
+- Each password is 15 characters long ✔ 
+
+All stretch goals (challenges) are implemented (recent commit): 
+
+- Ability to set password length ✔
+- Add "copy-on-click" ✔
+- Toggle "symbols" and "numbers" on/off ✔ + "letters"
--- index.js
+++ index.js
@@ -1,24 +1,78 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
 
+const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?", "/"];
+
+let checkedArray = [];
+let passwordGenerated = false;
+let oneChecked = false;
+let checkedMessage = document.getElementById("minimum-check");
+let copyMessage = document.getElementById("copy-message");
 let pw1 = document.getElementById("pw-1");
 let pw2 = document.getElementById("pw-2");
 
+function setCheckedArray() {
+    checkedArray = [];
+    passwordGenerated = false;
+    oneChecked = false;
+    copyMessage.textContent = "";
+    let letterCheck = document.getElementById("letters");
+    let numberCheck = document.getElementById("numbers");
+    let symbolCheck = document.getElementById("symbols");
+    
+    if (letterCheck.checked === true) {
+        checkedArray = checkedArray.concat(letters)
+        oneChecked = true;
+        checkedMessage.textContent = "";
+    }
+     if (numberCheck.checked === true) {
+        checkedArray = checkedArray.concat(numbers)
+        oneChecked = true;
+        checkedMessage.textContent = "";
+    }
+     if (symbolCheck.checked === true) {
+        checkedArray = checkedArray.concat(symbols)
+        oneChecked = true;
+        checkedMessage.textContent = "";
+    }
+    
+    if (oneChecked == false) {
+        checkedMessage.textContent = "You must select at least one box."
+    }
+}
+
 function selectRandomIndex(array) {
-    let randomIndex = Math.random()*characters.length;
+    let randomIndex = Math.random()*checkedArray.length;
     return Math.floor(randomIndex);
 }
 
-function generatePassword() {
-    let pwLength = 15;
-    let password = "";
-    for (let i = 0; i<pwLength; i++) {
-        password += characters[selectRandomIndex(characters)];
-    }
+function generatePassword(length) {
+    setCheckedArray();
+    if (oneChecked == true) {
+        let pwLength = length;
+	if (length >=4 && length <= 100) {
+        let password = "";
+        for (let i = 0; i<pwLength; i++) {
+            password += checkedArray[selectRandomIndex(checkedArray)];
+        }
+    passwordGenerated = true;
     return password;
+	}
+    }
 }
 
 function populateFields() {
-    pw1.textContent = generatePassword();
-    pw2.textContent = generatePassword();
+    let pwLength = document.getElementById("pw-length").value;
+    pw1.textContent = generatePassword(pwLength);
+    pw2.textContent = generatePassword(pwLength);
+}
+
+function copyToClipboard(id) {
+    if (passwordGenerated == true) {
+        document.getElementById(id).select();
+        document.execCommand('copy');
+        copyMessage.textContent = "Password copied to clipboard!";
+    }
 }
